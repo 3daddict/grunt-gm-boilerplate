e124c0f96ac08d9a1859df1f0f6b517ba5072051
3daddict <print@3daddict.com>
2019-03-06 09:21:49 -0800

    file structure added, readme updated

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,11 @@
 module.exports = function(grunt) {
     grunt.initConfig({
-      responsive_images: {
+      grunt_gm_boilerplate: {
         dev: {
           options: {
+            //Engine set to use GraphicsMagicks use "im" for ImageMagic
             engine: "gm",
+            //Name, suffix quality and width settings for image optimization and saving output
             sizes: [
               { name: "sm", suffix: "_1x", quality: 60, width: 600 },
               { name: "sm", suffix: "_2x", quality: 60, width: 1200 },
@@ -13,34 +15,40 @@ module.exports = function(grunt) {
               { name: "lg", suffix: "_2x", quality: 60, width: 2880 }
             ]
           },
+          //First operation settings to optimize any jpg, png or gif files in src dir
           files: [
             {
+              //settings
               expand: true,
-              src: ["**/*.{jpg,png}"],
-              cwd: "src/",
-              dest: "dest/"
+              src: ["**/*.{jpg,png,gif}"],
+              cwd: "src/images/",
+              dest: "dist/images/"
             }
           ]
         }
       },
+      //Second operation settings for saving images in output folder to webp versions
       cwebp: {
         dynamic: {
+          //Quality settings for webp conversion
           options: {
             q: 60
           },
           files: [
+            //settings
             {
               expand: true,
-              cwd: "dest/",
+              cwd: "dist/images/",
               src: ["**/*.{jpg,png}"],
-              dest: "dest/"
+              dest: "dist/images/"
             }
           ]
         }
       }
     });
+  //Grunt task runners
   grunt.loadNpmTasks("grunt-responsive-images");
-    grunt.loadNpmTasks("grunt-cwebp");
+  grunt.loadNpmTasks("grunt-cwebp");
   grunt.registerTask("default", ["responsive_images", "cwebp"]);
-  };
+};
 
--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# grunt-gm-boilerplate
-Grunt Compiler for Image Optimization using GraphicsMagick
+# Grunt Image Compiler
+Grunt Image Compiler for Image Optimization using GraphicsMagick.
+Running this script will optimize images in set folder.
+First operation images will first be compressed, optimized, resized and then saved with the set naming convention.
+Second operation images will be saved to web format and stored in same output directory.
+HTML and CSS Examples on how to setup <picture> images, breakpoints and fallback images.
+
+## Install Dependancies
+- GraphicsMagick `brew install graphicsmagick`
+- Grunt CLI `npm install -g grunt-cli`
+
+## Image Breakpoints
+#### Small - sm
+- 1x | quality: 60% | width: 600px
+- 2x | quality: 60% | width: 1200px
+
+#### Medium - md
+- 1x | quality: 60% | width: 900px
+- 2x | quality: 60% | width: 1800px
+
+#### Large - lg
+- 1x | quality: 60% | width: 1440px
+- 2x | quality: 60% | width: 2880px
+
+**Image settings can be adjusted in Gruntfile.js**
+
+### Naming convention
+Image names will be appended with the following attributes
+- Original filename: `example-image.jpg`
+- Compiled output filenames;
+`example-image-sm_1x.jpg,
+example-image-sm_2x.jpg,
+example-image-md_1x.jpg,
+example-image-md_2x.jpg,
+example-image-lg_1x.jpg,
+example-image-lg_2x.jpg
+example-image-sm_1x.webp,
+example-image-sm_2x.webp,
+example-image-md_1x.webp,
+example-image-md_2x.webp,
+example-image-lg_1x.webp,
+example-image-lg_2x.webp`
+
+### Default Fallback Images
+To ensure proper cross-browser compatibily original source files should also be optimized and then **always** set as the default fallback image
